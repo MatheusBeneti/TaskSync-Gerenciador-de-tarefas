c4d171871c89476506588afd4e983c098d3bc079
MatheusBeneti <matheusbeneti3@gmail.com>
2023-12-04 21:35:02 -0300

    adicionado handlebars, views, rota account

--- index.js
+++ index.js
@@ -1,80 +1,11 @@
-/*const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
-
-const path = require('path')
-
-const basePath = path.join(__dirname, 'templates')
-
-const users = require('./users')
-
-// ler o body
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-)
-
-app.use(express.json())
-
-app.use(express.static('public'))
-
-var checkAuth = function (req, res, next) {
-  req.authStatus = true
-
-  if (req.authStatus) {
-    console.log('Está logado, pode continuar')
-    next()
-  } else {
-    console.log('Não está logado, faça o login para continuar!')
-  }
-}
-
-app.use(checkAuth)
-
-app.use('/users', users)
-
-app.get('/', (req, res) => {
-  res.sendFile(`${basePath}/index.html`)
-})
-
-app.listen(port, () => {
-  console.log(`App rodando na porta:${port}`)
-})
-
-
-
-const http = require('http');
-const express = require('express');
-const app = express();
-const path = require('path'); // Importe o módulo 'path' para manipulação de caminhos
-const port = process.env.PORT || 3000;
-
-/*http.createServer(function(request, response) {
-  
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('Espanção de domínio, azure!');
-}).listen(port);
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'home.html');
-  res.sendFile(filePath);
-});
-
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
-*/
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
-const basePath = path.join(__dirname, 'templates');
+const basePath = path.join(__dirname, 'views');
 const router = require('./routers/routers');
+const exphbs = require('express-handlebars');
 
 const checkAuth = function(req, res, next){
 
@@ -89,6 +20,11 @@ const checkAuth = function(req, res, next){
     next();
   }
 }
+
+const hbs = exphbs.create({ /* configurações opcionais aqui */ });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
--- routers/routers.js
+++ routers/routers.js
@@ -4,16 +4,21 @@ const router = express.Router();
 const TaskController = require('../controllers/TaskController');
 
 const path = require('path');
-const basePath = path.join(__dirname, '..', 'Templates');
+const basePath = path.join(__dirname, '..', 'views');
 
 router.get('/', (req, res) => {
     res.sendFile(basePath + '/login.html');
 });
 
+//router.get('/', chamarFunction) 
+
 router.get('/home', (req, res) => {
     res.sendFile(basePath + '/home.html'); 
 });
 
+router.get('/account', (req, res) => {
+    res.sendFile(basePath + '/userAccount.handlebars'); 
+});
 
 module.exports = router;  
 
