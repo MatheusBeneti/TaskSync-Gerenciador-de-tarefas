987279062fddccd77f3bacfead2513bb39cb8433
Beneti <matheusbeneti2@gmail.com>
2023-12-06 12:55:52 -0300

    adicionado autenticação

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
 
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
+
 const path = require('path');
 const basePath = path.join(__dirname, 'views');
+
 const router = require('./routers/routers');
+
 const exphbs = require('express-handlebars');
-
-const checkAuth = function(req, res, next){
-
-  req.authStatus = true;//implementar function para validar firebase
-  if(req.authStatus){
-    console.log("Está logado com sucesso");
-    next();
-  }
-  else{
-    console.log("Não está logado");
-    res.sendFile(basePath + '/login.html');
-    next();
-  }
-}
-
 const hbs = exphbs.create({ /* configurações opcionais aqui */ });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
-
-app.use(checkAuth);
-
 app.use(router);
-
-app.use(function(req, res, next) {
-  res.status(404).sendFile(basePath + '/404.html');
-})
+app.use(function(req, res) {res.status(404).sendFile(basePath + '/404.html');})
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
--- routers/routers.js
+++ routers/routers.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const userAccountController = require('../controllers/userAccountPageController');
 const homeController = require('../controllers/homeController');
 const loginController = require('../controllers/loginController');
+const checkAuth = require('../controllers/checkAuthController');
 
-const path = require('path');
-const basePath = path.join(__dirname, '..', 'views');
+//const path = require('path');
+//const basePath = path.join(__dirname, '..', 'views');
 
 router.get('/', loginController)
-router.get('/home', homeController);
-router.get('/account', userAccountController);
+router.get('/home',checkAuth, homeController);
+router.get('/account',checkAuth, userAccountController);
 
 module.exports = router;  
 
