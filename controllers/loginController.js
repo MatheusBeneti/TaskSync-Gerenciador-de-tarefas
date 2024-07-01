ca18d5e478ff6a7b0e319a401bb2fb1c8be0837d
MatheusBeneti <matheusbeneti3@gmail.com>
2024-07-01 13:59:58 -0300

    bug-fix login internal error

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,40 +2,35 @@ const DbInteractions = require('../models/dbInteractions.js');
 const dbInteractions = new DbInteractions(); 
 const crypto = require('crypto');
 
-function hashPassword(password) {
-    const salt = crypto.randomBytes(16).toString('hex');
-    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
-    return {
-        salt: salt,
-        hash: hash
-    };
-}
-
-function verifyPassword(password, storedHash, storedSalt) {
-    const hash = crypto.pbkdf2Sync(password, storedSalt, 1000, 64, 'sha512').toString('hex');
-    return hash === storedHash;
+function verifyPassword(password, storedPassword) {
+    return password === storedPassword;
 }
 
 module.exports = class UserController {
     static loadPage = (req, res) => {
+        console.log('Loading login page');
         res.render('login', { layout: false });
     }
 
     static validateLogin = async (req, res) => {
         const userData = req.body;
+        console.log('Validating login for:', userData.email);
 
         try {
             const queryResult = await dbInteractions.getUserIdAndPassword(userData.email);
+            console.log('Query result:', queryResult);
 
             if (!queryResult) {
                 console.log('User not found');
                 return res.redirect('/');
             }
 
-            const passwordMatch = verifyPassword(userData.password, queryResult.userPassword, queryResult.salt);
+            const passwordMatch = verifyPassword(userData.password, queryResult.userPassword);
+            console.log('Password match:', passwordMatch);
 
             if (passwordMatch) {
                 req.session.userid = queryResult.userId;
+                console.log('User ID set in session:', req.session.userid);
                 req.session.save(() => {
                     res.redirect('/home');
                 });
@@ -52,29 +47,33 @@ module.exports = class UserController {
 
     static validateNewAccount = async (req, res) => {
         const userData = req.body;
+        console.log('Validating new account for:', userData.email);
 
         try {
             const queryResult = await dbInteractions.getUserIdAndPassword(userData.email);
+            console.log('Query result:', queryResult);
 
             if (queryResult !== null) {
                 res.redirect('/');
                 console.log('User exists');
             } else {
-                const { salt, hash } = hashPassword(userData.password);
-                const userId = await dbInteractions.addNewUser(userData.name, userData.email, hash, salt);
+                const userId = await dbInteractions.addNewUser(userData.name, userData.email, userData.password);
+                console.log('New user ID:', userId);
 
                 req.session.userid = userId;
+                console.log('User ID set in session:', req.session.userid);
                 req.session.save(() => {
                     res.redirect('/home');
                 });
             }
         } catch (error) {
-            console.error('Error:', error);
+            console.error('Error during account validation:', error);
             res.status(500).send('Internal Server Error');
         }
     };
 
     static logout = (req, res) => {
+        console.log('Logging out user:', req.session.userid);
         req.session.destroy(() => {
             res.redirect('/');
         });
